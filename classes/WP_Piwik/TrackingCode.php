dc40ed9c02593c03b7d24e6a1f6dc45cf8f03187
André Bräkling <webmaster@braekling.de>
2016-10-27 23:58:41 +0200

    Tracking code filter rename

--- classes/WP_Piwik/TrackingCode.php
+++ classes/WP_Piwik/TrackingCode.php
@@ -26,7 +26,7 @@ class TrackingCode {
 			$this->applySearchChanges ();
 		if (is_single () || is_page())
 			$this->addCustomValues ();
-		$this->trackingCode = apply_filters('wppiwik_tracking_code', $this->trackingCode);
+		$this->trackingCode = apply_filters('wp-piwik_tracking_code', $this->trackingCode);
 		return $this->trackingCode;
 	}
 
