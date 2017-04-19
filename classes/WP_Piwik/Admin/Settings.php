29eaa70bdb2aba6f1e7514baaa043dcd785414c3
André Bräkling <webmaster@braekling.de>
2017-04-19 19:41:49 +0200

    Flattr button update

--- classes/WP_Piwik/Admin/Settings.php
+++ classes/WP_Piwik/Admin/Settings.php
@@ -496,20 +496,8 @@ class Settings extends \WP_Piwik\Admin {
 	<p>
 		<?php _e('If you like WP-Piwik, you can support its development by a donation:', 'wp-piwik'); ?>
 	</p>
-	<script type="text/javascript">
-	/* <![CDATA[ */
-	window.onload = function() {
-		FlattrLoader.render({
-        	'uid': 'flattr',
-            'url': 'http://wp.local',
-            'title': 'Title of the thing',
-            'description': 'Description of the thing'
-		}, 'element_id', 'replace');
-	}
-	/* ]]> */
-	</script>
 	<div>
-		<a class="FlattrButton" style="display:none;" title="WordPress Plugin WP-Piwik" rel="flattr;uid:braekling;category:software;tags:wordpress,piwik,plugin,statistics;" href="https://www.braekling.de/wp-piwik-wpmu-piwik-wordpress">This WordPress plugin adds a Piwik stats site to your WordPress dashboard. It's also able to add the Piwik tracking code to your blog using wp_footer. You need a running Piwik installation and at least view access to your stats.</a>
+		<script id='fb0ahsp'>(function(i){var f,s=document.getElementById(i);f=document.createElement('iframe');f.src='//button.flattr.com/view/?fid=mkdp7z&url=https%3A%2F%2Fwww.braekling.de%2Fwp-piwik-wpmu-piwik-wordpress';f.title='Flattr';f.height=62;f.width=55;f.style.borderWidth=0;s.parentNode.insertBefore(f,s);})('fb0ahsp');</script>
 	</div>
 	<div>
 		Paypal
@@ -549,7 +537,6 @@ class Settings extends \WP_Piwik\Admin {
 	 */
 	public function extendAdminHeader() {
 		echo '<script type="text/javascript">var $j = jQuery.noConflict();</script>';
-		echo '<script type="text/javascript">/* <![CDATA[ */(function() {var s = document.createElement(\'script\');var t = document.getElementsByTagName(\'script\')[0];s.type = \'text/javascript\';s.async = true;s.src = \'//api.flattr.com/js/0.6/load.js?mode=auto\';t.parentNode.insertBefore(s, t);})();/* ]]> */</script>';
 	}
 
 	/**
